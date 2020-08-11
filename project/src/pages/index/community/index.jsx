61302a39b35714fae97629b10c1c4d9a714576d6
IMTJY <798149739@qq.com>
2020-08-11 14:46:17 +0800

    small change

--- project/src/pages/index/community/index.jsx
+++ project/src/pages/index/community/index.jsx
@@ -1,9 +1,22 @@
 import React, { Component } from 'react'
-import { View, Button, Text } from '@tarojs/components'
+import { View, Button, Text, ScrollView } from '@tarojs/components'
 
 export default class Community extends Component {
 
     render(){ 
-        return <View>123</View>
+        return (
+            <View>
+                <View>
+                    <View>topbar</View>
+                </View>
+                <View>
+                    <ScrollView>
+                        <View>
+                            boxs
+                        </View>
+                    </ScrollView>
+                </View>
+            </View>
+        )
     } 
 }
